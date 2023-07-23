f0c5c0fbdf61ebcc69f05fd3e395c328fadbc21d
TAKAHASHI Taro <takahashi.taro.keitai@gmail.com>
2023-07-23 11:44:36 +0900

    first commit

---
+++ Chapter03/python/lambda_function.py
@@ -0,0 +1,226 @@
+import logging
+import ask_sdk_core.utils as ask_utils
+
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components.request_components import AbstractRequestHandler
+from ask_sdk_core.dispatch_components.exception_components import (
+    AbstractExceptionHandler,
+)
+from ask_sdk_core.handler_input import HandlerInput
+from ask_sdk_model.dialog.elicit_slot_directive import ElicitSlotDirective
+from ask_sdk_model.intent import Intent
+from ask_sdk_model.intent_confirmation_status import IntentConfirmationStatus
+from ask_sdk_model.slot import Slot
+from ask_sdk_model.slot_confirmation_status import SlotConfirmationStatus
+
+from ask_sdk_model.response import Response
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class LaunchRequestHandler(AbstractRequestHandler):
+    """Handler for Skill Launch."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+
+        return ask_utils.is_request_type("LaunchRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "私はオウムです。あなたのマネをします。"
+
+        directive = ElicitSlotDirective(
+            slot_to_elicit="user_message",
+            updated_intent=Intent(
+                name="ParrotIntent",
+                confirmation_status=IntentConfirmationStatus.NONE,
+                slots={
+                    "user_message": Slot(
+                        name="user_message",
+                        value="",
+                        confirmation_status=SlotConfirmationStatus.NONE,
+                    )
+                },
+            ),
+        )
+
+        return (
+            handler_input.response_builder.speak(speak_output)
+            .ask("なにか話しかけてください。")
+            .add_directive(directive)
+            .response
+        )
+
+
+class ParrotIntentHandler(AbstractRequestHandler):
+    """Handler for Parrot Intent."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("ParrotIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        user_input = ask_utils.get_slot_value(
+            handler_input=handler_input, slot_name="user_message"
+        )
+        speak_output = user_input if isinstance(user_input, str) else ""
+
+        directive = ElicitSlotDirective(
+            slot_to_elicit="user_message",
+            updated_intent=Intent(
+                name="ParrotIntent",
+                confirmation_status=IntentConfirmationStatus.NONE,
+                slots={
+                    "user_message": Slot(
+                        name="user_message",
+                        value="",
+                        confirmation_status=SlotConfirmationStatus.NONE,
+                    )
+                },
+            ),
+        )
+
+        return (
+            handler_input.response_builder.speak(speak_output)
+            .ask("なにか話しかけてください。")
+            .add_directive(directive)
+            .response
+        )
+
+
+class HelpIntentHandler(AbstractRequestHandler):
+    """Handler for Help Intent."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.predicate.is_intent_name("AMAZON.HelpIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "自由に話しかけてみてください。"
+
+        return (
+            handler_input.response_builder.speak(speak_output)
+            .ask(speak_output)
+            .response
+        )
+
+
+class CancelOrStopIntentHandler(AbstractRequestHandler):
+    """Single handler for Cancel and Stop Intent."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.predicate.is_intent_name("AMAZON.CancelIntent")(
+            handler_input
+        ) or ask_utils.predicate.is_intent_name("AMAZON.StopIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "さようなら。"
+
+        return handler_input.response_builder.speak(speak_output).response
+
+
+class FallbackIntentHandler(AbstractRequestHandler):
+    """Single handler for Fallback Intent."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.predicate.is_intent_name("AMAZON.FallbackIntent")(
+            handler_input
+        )
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("In FallbackIntentHandler")
+        speech = "すみません、よくわかりません。"
+        reprompt = "お助けできることは何かありますか？"
+
+        return handler_input.response_builder.speak(speech).ask(reprompt).response
+
+
+class SessionEndedRequestHandler(AbstractRequestHandler):
+    """Handler for Session End."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.predicate.is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+
+        # Any cleanup logic goes here.
+
+        return handler_input.response_builder.response
+
+
+class IntentReflectorHandler(AbstractRequestHandler):
+    """The intent reflector is used for interaction model testing and debugging.
+    It will simply repeat the intent the user said. You can create custom handlers
+    for your intents by defining them above, then also adding them to the request
+    handler chain below.
+    """
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.predicate.is_request_type("IntentRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        intent_name = ask_utils.request_util.get_intent_name(handler_input)
+        speak_output = "あなたが呼び出したインテントはこちらです。{}".format(intent_name)
+
+        return (
+            handler_input.response_builder.speak(speak_output)
+            # .ask("add a reprompt if you want to keep the session open for the user to respond")
+            .response
+        )
+
+
+class CatchAllExceptionHandler(AbstractExceptionHandler):
+    """Generic error handling to capture any syntax or routing errors. If you receive an error
+    stating the request handler chain is not found, you have not implemented a handler for
+    the intent being invoked or included it in the skill builder below.
+    """
+
+    def can_handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> bool
+        return True
+
+    def handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> Response
+        logger.error(exception, exc_info=True)
+
+        speak_output = "申し訳ありません。もう一度お試しください。"
+
+        return (
+            handler_input.response_builder.speak(speak_output)
+            .ask(speak_output)
+            .response
+        )
+
+
+# The SkillBuilder object acts as the entry point for your skill, routing all request and response
+# payloads to the handlers above. Make sure any new handlers or interceptors you've
+# defined are included below. The order matters - they're processed top to bottom.
+
+
+sb = SkillBuilder()
+
+sb.add_request_handler(LaunchRequestHandler())
+sb.add_request_handler(ParrotIntentHandler())
+sb.add_request_handler(HelpIntentHandler())
+sb.add_request_handler(CancelOrStopIntentHandler())
+sb.add_request_handler(FallbackIntentHandler())
+sb.add_request_handler(SessionEndedRequestHandler())
+sb.add_request_handler(
+    IntentReflectorHandler()
+)  # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+
+sb.add_exception_handler(CatchAllExceptionHandler())
+
+lambda_handler = sb.lambda_handler()
---
+++ Chapter05/nodejs/index.js
@@ -0,0 +1,211 @@
+const Alexa = require('ask-sdk-core');
+const { Configuration, OpenAIApi } = require('openai');
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speakOutput = '私はAIチャットボットです。何でも聞いてください。';
+        const slotToElicit = 'user_message';
+        const intent = {
+            name: 'ChatBotIntent',
+            confirmationStatus: 'NONE',
+            slots: {
+                user_message: {
+                    name: 'user_message',
+                    confirmationStatus: 'NONE',
+                    value: ''
+                }
+            }
+        };
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt('なにか話しかけてください。')
+            .addElicitSlotDirective(slotToElicit, intent)
+            .getResponse();
+    }
+};
+
+const ChatBotIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'ChatBotIntent';
+    },
+    async handle(handlerInput) {
+        const user_input = Alexa.getSlotValue(handlerInput.requestEnvelope, 'user_message');
+
+        const configuration = new Configuration({
+            apiKey: 'your-api-key',
+        });
+        const openai = new OpenAIApi(configuration);
+        const template = "あなたは音声対話型チャットボットです。以下の制約にしたがって回答してください。\n\n制約:\n- ユーザーのメッセージに句読点を補ってから回答します\n- 簡潔な短い文章で話します\n- 質問の答えがわからない場合は「わかりません」と答えます";
+        let messages = [
+            {"role": "system", "content": template}
+        ];
+        if (handlerInput.attributesManager.getSessionAttributes().MESSAGES) {
+            messages = handlerInput.attributesManager.getSessionAttributes().MESSAGES;
+        }
+        messages.push({
+            "role": "user",
+            "content": user_input || "こんにちは"
+        });
+        try {
+            const completion = await openai.createChatCompletion({
+                model: "gpt-3.5-turbo",
+                messages: messages
+            });
+            const speakOutput = completion.data.choices[0].message.content;
+            handlerInput.attributesManager.setSessionAttributes({MESSAGES: messages});
+            messages.push({
+                "role": "assistant",
+                "content": speakOutput
+            });
+            const slotToElicit = 'user_message';
+            const intent = {
+                name: 'ChatBotIntent',
+                confirmationStatus: 'NONE',
+                slots: {
+                    user_message: {
+                        name: 'user_message',
+                        confirmationStatus: 'NONE',
+                        value: ''
+                    }
+                }
+            };
+            return handlerInput.responseBuilder
+                .speak(speakOutput)
+                .reprompt('なにか話しかけてください。')
+                .addElicitSlotDirective(slotToElicit, intent)
+                .getResponse();
+        } catch (error) {
+            console.error("Fiiled to call OpenAI API: ", error);
+            return handlerInput.responseBuilder
+                .speak("すみません、エラーが発生しました。しばらく時間をおいてからもう一度お試しください。")
+                .getResponse();
+        }
+    }
+};
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = '自由に話しかけてみてください。';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speakOutput = 'さようなら。';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * FallbackIntent triggers when a customer says something that doesn’t map to any intents in your skill
+ * It must also be defined in the language model (if the locale supports it)
+ * This handler can be safely added but will be ingnored in locales that do not support it yet 
+ * */
+const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'すみません、よくわかりません。';
+        const reprompt = 'お助けできることは何かありますか？';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
+ * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
+ * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
+ * */
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
+    }
+};
+/* *
+ * The intent reflector is used for interaction model testing and debugging.
+ * It will simply repeat the intent the user said. You can create custom handlers for your intents 
+ * by defining them above, then also adding them to the request handler chain below 
+ * */
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `あなたが呼び出したインテントはこちらです。${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+/**
+ * Generic error handling to capture any syntax or routing errors. If you receive an error
+ * stating the request handler chain is not found, you have not implemented a handler for
+ * the intent being invoked or included it in the skill builder below 
+ * */
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const speakOutput = '申し訳ありません。もう一度お試しください。';
+        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+/**
+ * This handler acts as the entry point for your skill, routing all request and response
+ * payloads to the handlers above. Make sure any new handlers or interceptors you've
+ * defined are included below. The order matters - they're processed top to bottom 
+ * */
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        ChatBotIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        FallbackIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler)
+    .addErrorHandlers(
+        ErrorHandler)
+    .withCustomUserAgent('chapter05')
+    .lambda();
